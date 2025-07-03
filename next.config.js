ee79779de222fc64718aeda497e640c676057396
Akif Azher Qureshi <akifazherq@gmail.com>
2025-07-03 04:06:13 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/Portfolio-website',
+  assetPrefix: '/Portfolio-website/',
+  images: {
+    unoptimized: true,
+  },
 }; 
